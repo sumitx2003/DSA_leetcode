b23b4b75e9ada20ea100ff55306b969c83e4c69c
Sumit  jangid <amitjangir13122004@gmail.com>
2026-07-30 03:14:07 +0530

    Time: 0 ms (100.00%) | Memory: 18.5 MB (68.10%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.cpp
+++ 101-symmetric-tree/symmetric-tree.cpp
@@ -11,38 +11,68 @@
  */
 class Solution {
 public:
-         string s1="";
-         string s2="";
-    void dfs(TreeNode* root){
-        if(root==NULL){
+    string s1;
+    string s2;
+    void dfs(TreeNode* node){
+        if(node->left==NULL && node->right==NULL){
+            s1+=node->val+'0';
+            return ;
+        }
+
+        if(node==NULL){
             s1+="*";
+        }
+        else{
+            s1+=node->val+'0';
+        }
+        if(!node->left && node->right){
+            s1+='*';
+        }
+    
+        if(node->left)dfs(node->left);
+
+         if(!node->right && node->left){
+            s1+='*';
+        }
+
+        if(node->right)dfs(node->right);
+    }
+      void dfs1(TreeNode* node){
+        if(node->left==NULL && node->right==NULL){
+            s2+=node->val+'0';
             return ;
         }
-        s1+=to_string(root->val);
 
-        dfs(root->left);
-        dfs(root->right);
-
-    }
-    void dfs1(TreeNode* root){
-        if(root==NULL){
+        if(node==NULL){
             s2+="*";
-            return ;
         }
-        s2+=to_string(root->val);
+        else{
+            s2+=node->val+'0';
+        }
 
-        dfs1(root->right);
-        dfs1(root->left);
+        if(!node->right && node->left)s2+='*';
+
+        if(node->right)dfs1(node->right);
+
+        if(!node->left && node->right)s2+='*';
+
+        if(node->left)dfs1(node->left);
 
     }
+
     bool isSymmetric(TreeNode* root) {
         if(root==NULL)return true;
-         
-         dfs(root->left);
-         dfs1(root->right);
 
-         if(s1==s2)return true;
+        if(!root->left && !root->right)return true;
 
-         return false; 
+        if((!root->left && root->right) || (root->left && !root->right))return false;
+
+        s1="";
+        s2="";
+
+        dfs(root->left);
+        dfs1(root->right);
+
+        return s1==s2;
     }
 };
