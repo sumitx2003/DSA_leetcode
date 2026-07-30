e7e33737d871d15aea3350c6d5239b733502e6ce
Sumit  jangid <amitjangir13122004@gmail.com>
2026-07-30 23:59:22 +0530

    Time: 113 ms (5.13%) | Memory: 69.1 MB (5.82%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
@@ -9,19 +9,62 @@
  */
 class Solution {
 public:
+    
+    void solve(TreeNode* root,unordered_map<TreeNode*,TreeNode*>&par,vector<TreeNode*>&ans){
+        if(!root)return;
+
+        if(par.find(root)!=par.end() && par[root]!=NULL){
+            ans.push_back(par[root]);
+        }
+        solve(par[root],par,ans);
+    }
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if(root==NULL)return NULL;
-
         if(root==p || root==q)return root;
+        
+        unordered_map<TreeNode*,TreeNode*>par;
+        queue<TreeNode*>pq;
+        if(!root)return NULL;
+        par[root]=NULL;
 
-        TreeNode* leftN=lowestCommonAncestor(root->left,p,q);
-        TreeNode* rightN=lowestCommonAncestor(root->right,p,q);
+        pq.push(root);
 
-        if(leftN!=NULL && rightN!=NULL)return root;
+        while(!pq.empty()){
+            int N=pq.size();
+            while(N--){
+                TreeNode* node=pq.front();
+                pq.pop();
 
-        if(leftN!=NULL)return leftN;
+                if(node->left){
+                    pq.push(node->left);
+                    par[node->left]=node;
+                }
+                if(node->right){
+                    pq.push(node->right);
+                    par[node->right]=node;
+                }
+            }
+        }
+        unordered_set<TreeNode*>st1;
+        vector<TreeNode*>first;
+        vector<TreeNode*>second;
+    
+        solve(p,par,first);
+        solve(q,par,second);
 
-        return rightN;
+        reverse(first.begin(),first.end());
+        reverse(second.begin(),second.end());
+        first.push_back(p);
+        second.push_back(q);
+        reverse(first.begin(),first.end());
+        reverse(second.begin(),second.end());
 
+        for(int i=0;i<first.size();i++){
+            st1.insert(first[i]);
+        }
+        for(int i=0;i<second.size();i++){
+            if(st1.find(second[i])!=st1.end())return second[i];
+        }
+
+        return  root;
     }
 };
