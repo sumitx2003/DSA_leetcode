c29a8486eb6bfa9a4aa7a83c9288dbd87ea69f80
Sumit  jangid <amitjangir13122004@gmail.com>
2026-08-12 20:17:22 +0530

    Time: 0 ms (100.00%) | Memory: 25 MB (49.14%) - LeetSync

--- 513-find-bottom-left-tree-value/find-bottom-left-tree-value.cpp
+++ 513-find-bottom-left-tree-value/find-bottom-left-tree-value.cpp
@@ -12,31 +12,14 @@
 class Solution {
 public:
     int findBottomLeftValue(TreeNode* root) {
+        int low=root->val;
+
         queue<TreeNode*>q;
         q.push(root);
-        int level=0;
+        
         while(!q.empty()){
             int N=q.size();
-            level++;
-            while(N--){
-                TreeNode* node=q.front();
-                q.pop();
-                
-                if(node->left)q.push(node->left);
-
-                if(node->right)q.push(node->right);
-
-            }
-        }
-        int count=0;
-        q.push(root);
-        while(!q.empty()){
-            count++;
-            int N=q.size();
-            if(count==level){
-                TreeNode* temp=q.front();
-                return temp->val;
-            }
+            low=q.front()->val;
             while(N--){
                 TreeNode* node=q.front();
                 q.pop();
@@ -46,7 +29,6 @@ public:
                 if(node->right)q.push(node->right);
             }
         }
-        return root->val;
-
+        return low;
     }
 };
