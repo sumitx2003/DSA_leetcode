9369e2c1279627bbb39fa29230581197fe17c4c1
Sumit  jangid <amitjangir13122004@gmail.com>
2026-08-31 20:43:05 +0530

    Time: 5 ms (15.67%) | Memory: 115.8 MB (11.50%) - LeetSync

---
+++ 334-increasing-triplet-subsequence/increasing-triplet-subsequence.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool increasingTriplet(vector<int>& nums) {
+        int mini=INT_MAX;
+        int mid=INT_MAX;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]<mini){
+                mini=nums[i];
+            }
+            else if(nums[i]>mini && nums[i]<=mid){
+                mid=nums[i];
+            }
+            else if(nums[i]>mini && nums[i]>mid){
+                return true;
+            }
+        }
+        return false;
+    }
+};
