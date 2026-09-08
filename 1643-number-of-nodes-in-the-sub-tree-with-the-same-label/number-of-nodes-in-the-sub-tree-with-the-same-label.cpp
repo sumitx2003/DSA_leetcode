572b567b7298f02d2ae5bb1b4a246cfa2e6eae34
Sumit  jangid <amitjangir13122004@gmail.com>
2026-09-08 23:10:56 +0530

    Time: 186 ms (76.50%) | Memory: 155.1 MB (82.61%) - LeetSync

--- 1643-number-of-nodes-in-the-sub-tree-with-the-same-label/number-of-nodes-in-the-sub-tree-with-the-same-label.cpp
+++ 1643-number-of-nodes-in-the-sub-tree-with-the-same-label/number-of-nodes-in-the-sub-tree-with-the-same-label.cpp
@@ -1,36 +1,33 @@
 class Solution {
 public:
-    void solve(unordered_map<int,vector<int>>&mp,int curr,int parent,string &labels,vector<int>&res,vector<int>&count){
-         char mylabel=labels[curr];
-
-         int before=count[mylabel-'a'];
-
-         count[mylabel-'a']+=1;
-
-         for(int i=0;i<mp[curr].size();i++){
-              int neigh=mp[curr][i];
-
-              if(neigh==parent)continue;
-
-              solve(mp,neigh,curr,labels,res,count);
-         }
-         int after=count[mylabel-'a'];
-         res[curr]=after-before;
+    unordered_map<int,vector<int>>mp;
+    string label="";
+   void solve(int curr,int parent,vector<int>&count,vector<int>&res){
+        char ch=label[curr];
+        int before=count[ch-'a'];
+        count[ch-'a']+=1;
+        for(int i=0;i<mp[curr].size();i++){
+            int neigh=mp[curr][i];
+            if(neigh==parent)continue;
+            solve(neigh,curr,count,res);
+        }
+        int after=count[ch-'a'];
+        res[curr]=after-before;
     }
     vector<int> countSubTrees(int n, vector<vector<int>>& edges, string labels) {
-        unordered_map<int,vector<int>>mp;
+        
         for(int i=0;i<edges.size();i++){
-            int u=edges[i][0];
-            int v=edges[i][1];
+              int u=edges[i][0];
+              int v=edges[i][1];
 
-            mp[u].push_back(v);
-            mp[v].push_back(u);
+              mp[u].push_back(v);
+              mp[v].push_back(u);
         }
+        label=labels;
+        vector<int>res(n,0);
+        vector<int>count(26,0);
 
-         vector<int>res(n,0);
-         vector<int>count(26,0);
-         solve(mp,0,-1,labels,res,count);
-
-         return res;
+        solve(0,-1,count,res);
+        return res;
     }
 };
